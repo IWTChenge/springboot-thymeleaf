06c42dcd290c57aade29d1bd442f3b8d0e64b06f
lee <j1297092693@hotmail.com>
2020-03-15 22:38:24 +0800

    3/15

--- springboot-thymeleaf/src/main/java/com/thymeleaf/springbootthymeleaf/SpringbootThymeleafApplication.java
+++ springboot-thymeleaf/src/main/java/com/thymeleaf/springbootthymeleaf/SpringbootThymeleafApplication.java
@@ -1,9 +1,11 @@
 package com.thymeleaf.springbootthymeleaf;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.thymeleaf.springbootthymeleaf.dao")
 public class SpringbootThymeleafApplication {
 
     public static void main(String[] args) {
--- springboot-thymeleaf/src/main/java/com/thymeleaf/springbootthymeleaf/controller/ThymeleafController.java
+++ springboot-thymeleaf/src/main/java/com/thymeleaf/springbootthymeleaf/controller/ThymeleafController.java
@@ -1,21 +1,38 @@
 package com.thymeleaf.springbootthymeleaf.controller;
 
-import com.thymeleaf.springbootthymeleaf.entiy.User;
+import com.thymeleaf.springbootthymeleaf.entiy.Student;
+import com.thymeleaf.springbootthymeleaf.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import java.util.List;
+
 
 @Controller
 public class ThymeleafController {
-    @RequestMapping(value = "", method = RequestMethod.GET)
+    private StudentService ss;
+    @Autowired
+    public void setStudentService(StudentService studentservice){
+        this.ss=studentservice;
+    }
+    @RequestMapping(value = "/", method = RequestMethod.GET)
     public String show(Model model){
-        User user=new User();
-        user.setId("9527");
-        user.setName("007");
-        model.addAttribute("user",user);
+      List<Student> ls= ss.selectAll();
+        model.addAttribute("ls",ls);
         //model.addAttribute("name","chenge");
         return "index";
+    }
+    @RequestMapping(value = "welcome", method = RequestMethod.GET)
+    public String welcome(Model model){
+
+        return "login";
+    }
+    @RequestMapping(value = "login", method = RequestMethod.POST)
+    public String login(Model model){
+
+        return "login";
     }
 }
